a52e63b5f60f085b2663fe203850f7540db93598
Gorilla99-max <nmriza3@gmail.com>
2024-07-17 10:30:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7337406281:AAFvNIF5DobCUz06KTnmTs4nj9dm48g0Szw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("abdulvasitkhan")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6039703925")
 
 
 
